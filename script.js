0fc3b04fa442f3861fae4f066a37b59cdf9e4181
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-08-02 23:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const itemList = Array.from(document.getElementsByTagName("li"));
+
+for(let i=0; i < itemList.length; i++){
+	if(itemList[i].id){
+		if(itemList[i].id === "level"){
+			alert(`The level of the element is: ${i+1}`);
+		}
+	}
+}
